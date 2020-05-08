e7560c23949f74d8c342cd7649f74d8737970d01
Diego Sanchez <diegosandmg@gmail.com>
2020-05-08 18:49:26 -0400

    Markdown App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import marked from 'marked';
+import hljs from 'highlight.js' ;
 
 function App() {
+  marked.setOptions({
+    breaks: true,
+    highlight: function(code, language) {
+      const hljs = require('highlight.js');
+      const validLanguage = hljs.getLanguage(language) ? language : 'markdown';
+      return hljs.highlight(validLanguage, code).value;
+    }
+  })
+  const initialMarkDown = 
+`
+# H1
+## H2
+
+[I'm an inline-style link](https://www.google.com)
+
+Inline \`code\` has \`back-ticks around\` it.
+
+\`\`\`javascript
+// Codeblock
+var s = "JavaScript syntax highlighting";
+alert(s);
+\`\`\`
+
+1. First ordered list item
+2. Another item
+⋅⋅* Unordered sub-list. 
+1. Actual numbers don't matter, just that it's a number
+⋅⋅1. Ordered sub-list
+4. And another item.
+
+> Blockquotes are very handy in email to emulate reply text.
+> This line is part of the same quote.
+
+Strong emphasis, aka bold, with **asterisks** or __underscores__.
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
+  const [html, setHtml] = useState(
+    marked(initialMarkDown)
+  );
+  function processMarkdown(event){
+    setHtml(
+      marked(event.target.value)
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id="editor" onChange={processMarkdown}>
+        {initialMarkDown}
+      </textarea>
+      <section id="preview" dangerouslySetInnerHTML={{ __html: html}}>
+      </section>
     </div>
   );
 }
